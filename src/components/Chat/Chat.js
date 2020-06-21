4fc8b754f33cd783fd97251ba1b693ad628faf57
Vishal Manwani <vpmanwani2204@gmail.com>
2020-06-21 21:07:24 +0530

    Add files via upload
    first commit

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
+
+import './Chat.css';
+
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const ENDPOINT = 'https://chatter-patter.herokuapp.com/';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setRoom(room);
+        setName(name);
+
+        socket.emit('join', { name, room }, () => {
+        });
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }     
+    }, [ENDPOINT, location.search]);
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages((messages) => [...messages, message]);
+        });
+    }, []);
+
+    //function for sending messages
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if(message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    };
+
+    // console.log(message, messages); 
+
+    return (
+        <div className="outerContainer">
+            <div className="container">
+                <InfoBar room={room} />
+                <Messages messages={messages} name={name} />
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+                {/* <input value={message} onChange= {(event) => setMessage(event.target.value)} onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null} /> */}
+            </div>
+        </div>
+    );
+};
+
+export default Chat;
